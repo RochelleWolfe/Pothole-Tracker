bc9db2533008890ef50d668f4002a5afe575b3a5
Jordan Stiver <stiver.jordan@gmail.com>
2018-12-12 11:20:25 -0500

    push 1.21

--- database/schema.sql
+++ database/schema.sql
@@ -7,6 +7,9 @@ BEGIN;
 -- CREATE statements go here
 DROP TABLE IF EXISTS app_user;
 DROP TABLE IF EXISTS pothole;
+DROP TABLE IF EXISTS user_potholes;
+
+
 
 CREATE TABLE app_user (
   id            SERIAL PRIMARY KEY,
@@ -17,7 +20,7 @@ CREATE TABLE app_user (
 );
 
 CREATE TABLE pothole(
-        marker_id       varchar(32)      PRIMARY KEY,
+        marker_id       SERIAL      PRIMARY KEY,
         lat             varchar(32),
         long            varchar(32),
         img             varchar(100),
@@ -30,6 +33,9 @@ CREATE TABLE user_potholes(
         marker_id       varchar(32)
         );        
         
-
+INSERT into pothole (lat, long, img, street_add, size) 
+        values      (41.4156, -81.9235, null , 28206, 4);
+INSERT into pothole (lat, long, img, street_add, size) 
+        values      (41.3750, -81.9085, null , 44555, 3);
 
 COMMIT;
--- src/main/java/com/techelevator/controller/HomeController.java
+++ src/main/java/com/techelevator/controller/HomeController.java
@@ -1,22 +1,33 @@
 package com.techelevator.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.techelevator.model.Pothole;
+import com.techelevator.model.PotholeDao;
 
 @Controller
 @RequestMapping(value = "/users")
 public class HomeController {
 
+	private PotholeDao potholeDao;
 	
 	@RequestMapping(path="/{userName}", method=RequestMethod.GET)
 	public String displayUserHome(@PathVariable String userName,
 										HttpSession session) {
+		
+		List<Pothole> potholeList = potholeDao.getAllPotholes();
+		session.setAttribute("potholeList", potholeList);
+		
 		return "home";
 	}
+
+	
 	
 }
