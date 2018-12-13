623d30d8bb7ce6989b11b98c961f50a5ab95dadc
Rochelle Wolfe <rochellewolfe@ymail.com>
2018-12-13 13:24:11 -0500

    added img files, updated tables

--- database/schema.sql
+++ database/schema.sql
@@ -12,11 +12,11 @@ DROP TABLE IF EXISTS user_potholes;
 
 
 CREATE TABLE app_user (
-  id            SERIAL PRIMARY KEY,
-  user_name     varchar(32) NOT NULL    UNIQUE,
-  password      varchar(32) NOT NULL,
+  id            SERIAL          PRIMARY KEY,
+  user_name     varchar(32)     NOT NULL    UNIQUE,
+  password      varchar(32)     NOT NULL,
   role          varchar(32),
-  salt          varchar(255) NOT NULL
+  salt          varchar(255)    NOT NULL
 );
 
 CREATE TABLE pothole(
@@ -25,7 +25,9 @@ CREATE TABLE pothole(
         long            varchar(32),
         img             varchar(100),
         street_add      varchar(50),
-        size            int
+        size            int,
+        depth           int,
+        severity        int
 );
 
 CREATE TABLE user_potholes(
@@ -33,15 +35,15 @@ CREATE TABLE user_potholes(
         marker_id       varchar(32)
         );        
         
-INSERT into pothole (lat, long, img, street_add, size) 
-        values      (41.4156, -81.9235, 'abc', 28206, 4);
-INSERT into pothole (lat, long, img, street_add, size) 
-        values      (41.3750, -81.9085, 'def' , 44555, 3);
-INSERT into pothole (lat, long, img, street_add, size) 
-        values      (41.502675, -81.639509, 'ghi' , 4567, 2);
-INSERT into pothole (lat, long, img, street_add, size) 
-        values      (41.503679,  -81.640754, 'jkl' , 4567, 5);
-INSERT into pothole (lat, long, img, street_add, size) 
-        values      (41.504681,  -81.639509, 'mno' , 4567, 1);
+INSERT into pothole (lat,       long,   img,  street_add, size, depth, severity) 
+        values      (41.4156, -81.9235, 'abc', 28206,     4,      6,    1);
+INSERT into pothole (lat,       long,   img,  street_add, size, depth, severity)
+        values      (41.3750, -81.9085, 'def' , 44555,    3,      1,    2);
+INSERT into pothole (lat,       long,   img,  street_add, size, depth, severity)
+        values      (41.502675, -81.639509, 'ghi' , 4567, 2,       24,  3);
+INSERT into pothole (lat,       long,   img,  street_add, size, depth, severity) 
+        values      (41.503679,  -81.640754, 'jkl' , 4567, 5,       12,    1);
+INSERT into pothole (lat,       long,   img,  street_add, size, depth, severity)
+        values      (41.504681,  -81.639509, 'mno' , 4567, 1,      18,    2);
 
 COMMIT;
